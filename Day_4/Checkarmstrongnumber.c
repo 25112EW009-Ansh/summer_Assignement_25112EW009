7eab50d2613904f4beed8527d4dc76d014fe8634
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-05 13:42:11 +0530

    Implement Armstrong number check in C

--- Day_4/Checkarmstrongnumber.c
+++ Day_4/Checkarmstrongnumber.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n,rem,temp,sum=0;
+    printf("Enter the value of n.");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        rem=n%10;
+        sum=sum+rem*rem*rem;
+        n=n/10;
+    }
+    if(sum==temp)
+    {
+        printf("The given number is a Armstrong number ");
+    }
+    else{
+        printf("The given number is not armstrong ");
+    }
+
+    return 0;
+}
