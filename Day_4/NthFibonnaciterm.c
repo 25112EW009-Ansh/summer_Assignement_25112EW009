e57d7208ae7e68589f82969b23623f9d4fdd16fb
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-05 13:17:57 +0530

    Correct Fibonacci sequence calculation
    Fix Fibonacci calculation logic to correctly compute the Nth term.

--- Day_4/NthFibonnaciterm.c
+++ Day_4/NthFibonnaciterm.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int n,x=0,y=1,z=0;
+    printf("Enter the value of n.");
+    scanf("%d",&n);
+    while(z<=n)
+    {
+       
+       x=y;
+       y=z;
+       z=x+y;
+    }
+    printf("%d",z);
+
+    return 0;
+}
