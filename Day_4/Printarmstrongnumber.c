2c9db4a411e9177f7d5855bc78eb519853c93723
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-06 12:44:12 +0530

    Implement Armstrong number printer
    Added functionality to print Armstrong numbers within a specified range.

--- Day_4/Printarmstrongnumber.c
+++ Day_4/Printarmstrongnumber.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int i,startr,endr,temp,rem,sum=0.0;
+    printf("Enter the starting range:.");
+    scanf("%d",&startr);
+    printf("Enter the ending range:.");
+    scanf("%d",&endr);
+    for(i=startr;i<=endr;i++)
+    {
+        temp=i;
+        while(temp>0){
+            rem=temp%10;
+            sum=sum+rem*rem*rem;
+            temp=temp/10;
+        }
+        if(sum==i){
+            printf("%d",i);
+        }
+    }
+
+    return 0;
