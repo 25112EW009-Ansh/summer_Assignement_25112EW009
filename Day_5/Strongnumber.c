439e10a6f6c4d18bb4dcad571f195b2adb4a2016
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-06 14:27:28 +0530

    Implement strong number logic in Strongnumber.c
    Fix calculation of strong number by correcting factorial logic and sum accumulation.

--- Day_5/Strongnumber.c
+++ Day_5/Strongnumber.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int i,n,rem,fact=1,sum=0.0;
+    printf("Enter the number.");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        for(i=1;i<rem;i++)
+        {
+            fact=fact*i;
+            sum=sum+fact;
+        }
+        n=n/10;
+    }
+    if(sum==n)
+    {
+        printf("The number is STRONG NUMBER");
+    }
+    else
+    {
+        printf("The number is not STRONG NUMBER");
+    }
+
+    return 0;
+}
