d316a377f6af57ac15190d1a90fe9d55968ad1c7
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-07 13:01:17 +0530

    Implement prime factorization in Primefactor.c

--- Day_5/Primefactor.c
+++ Day_5/Primefactor.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int i,n,temp;
+    printf("Enter the number.");
+    scanf("%d",&n);
+    temp=n;
+    i=2;
+    while(temp>1)
+    {
+        if(temp%i==0)
+        {
+            printf("%d\n",i);
+            temp=temp/i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    return 0;
+}
