471eadee25d0c2539fc09b964924a069cf323554
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-08 13:36:11 +0530

    Implement function to count set bits in a number

--- Day_6/Countsetbit.c
+++ Day_6/Countsetbit.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int countSetBits(unsigned int n) {
+    int count = 0;
+    while (n > 0) {
+        n = n & (n - 1);
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    unsigned int num = 29; 
+    printf("Number of set bits in %u is: %d\n", num, countSetBits(num));
+    return 0;
+}
 
