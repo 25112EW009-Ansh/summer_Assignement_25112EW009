76320799ebea4ed6d973cc40f6e11309b831955a
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-08 13:50:01 +0530

    Implement power calculation for X raised to n

--- Day_6/X^n.c
+++ Day_6/X^n.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int X,n,i,power=1;
+    printf("Enter the value of X.");
+    scanf("%d",&X);
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        power=power*X;
+    }
+    printf("%d",power);
+
+    return 0;
+}
