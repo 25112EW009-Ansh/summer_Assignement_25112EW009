6ec19980223574437019c53170da321e3517cb49
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-10 13:11:28 +0530

    Implement alphabet pattern printing program
    Add a program to print an alphabet pattern based on user input.

--- Day_8/AlphabetPattern.c
+++ Day_8/AlphabetPattern.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int i,j,n;
+    printf("Enter the value of n.");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a=1;
+        for(j=1;j<=i;j++)
+        {
+            int d=a+64;
+            char ch=(char)d;
+    
+            printf("%c",ch);
+            a=a+1;
+            
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
