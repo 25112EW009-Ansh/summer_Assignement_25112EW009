1787e2e743667fd46484c5ef2216240df2f1169b
25112EW009-Ansh <25112ew009@glbitm.ac.in>
2026-06-27 13:39:10 +0530

    Add program to compute row-wise sum of a matrix

---
+++ Day_20/Rowwisesumsum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Row-wise sum:\n");
+    for (int i = 0; i < rows; i++) {
+        int row_sum = 0;
+        for (int j = 0; j < cols; j++) {
+            row_sum += matrix[i][j];
+        }
+        printf("Row %d: %d\n", i + 1, row_sum);
+    }
+
+    return 0;
+}
